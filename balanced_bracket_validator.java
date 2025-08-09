6f24e5e525d37f90c4654cccedd90dab2e9f73ec
ashi-ish <parulekar.ashish17@gmail.com>
2025-08-08 23:19:46 -0400

    changes

---
+++ balanced_bracket_validator.java
@@ -0,0 +1,15 @@
+// Live Coding Question 2: “Balanced Brackets Validator”
+// Category: Stack + Collections
+// Estimated Time: 15 minutes
+// Difficulty: Intermediate
+// Problem Statement:
+// Write a method that checks whether a given string has balanced brackets.
+// The string may contain (), {}, and []. All brackets must be closed in the
+// correct order.
+// Function Signature:
+// public boolean isBalanced(String expression);
+
+// Example:
+// isBalanced("{[()]}") => true
+// isBalanced("{[(])}") => false
+// isBalanced("((()))") => true
--- string_compression.java
+++ string_compression.java
@@ -13,19 +13,3 @@
 // Example:
 // compress("aabcccccaaa") => "a2b1c5a3"
 // compress("abc") => "abc"
-
-// Live Coding Question 2: “Balanced Brackets Validator”
-// Category: Stack + Collections
-// Estimated Time: 15 minutes
-// Difficulty: Intermediate
-// Problem Statement:
-// Write a method that checks whether a given string has balanced brackets.
-// The string may contain (), {}, and []. All brackets must be closed in the
-// correct order.
-// Function Signature:
-// public boolean isBalanced(String expression);
-
-// Example:
-// isBalanced("{[()]}") => true
-// isBalanced("{[(])}") => false
-// isBalanced("((()))") => true
